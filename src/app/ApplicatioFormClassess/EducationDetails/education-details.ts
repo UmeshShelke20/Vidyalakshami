e26f9a0e4f802b3e99406437971aad8c0366b546
UmeshShelke20 <urshelke20@gmail.com>
2023-08-19 06:51:09 +0530

    form field add

--- src/app/ApplicatioFormClassess/EducationDetails/education-details.ts
+++ src/app/ApplicatioFormClassess/EducationDetails/education-details.ts
@@ -1,2 +1,12 @@
+import { College } from "../College/college"
+
 export class EducationDetails {
+
+    eid:number
+	type:string
+	country:string
+	duration:string
+	fees :number
+	institutename:string
+	 college:College
 }
--- src/app/Modules/RlationMangaer/relation-manager/new-application/new-application.component.ts
+++ src/app/Modules/RlationMangaer/relation-manager/new-application/new-application.component.ts
@@ -20,6 +20,22 @@ export class NewApplicationComponent implements OnInit {
     branchName:new FormControl('',[Validators.required]),
 
   })
+
+  College=new FormGroup({
+    collegeId:new FormControl('',[Validators.required]),
+    collegeName:new FormControl('',[Validators.required]),
+    collegeEmail:new FormControl('',[Validators.required]),
+    collegeMobile:new FormControl('',[Validators.required]),
+    collegeaddress:new FormGroup({
+      id:new FormControl('',[Validators.required]),
+      district:new FormControl('',[Validators.required]),
+      state:new FormControl('',[Validators.required]),
+      city:new FormControl('',[Validators.required]),
+      landmark:new FormControl('',[Validators.required]),
+      blockno:new FormControl('',[Validators.required]),
+      })
+  })
+
   ngOnInit(): void {
 this.application=new FormGroup({
   applicationId:new FormControl('',[Validators.required]),
@@ -51,15 +67,28 @@ this.application=new FormGroup({
 	occupation:new FormControl('',[Validators.required]),
     panno:new FormControl('',[Validators.required]),
 	relationWithStudent:new FormControl('',[Validators.required]),
-  address:this.address,
+  address:new FormGroup({
+    id:new FormControl('',[Validators.required]),
+    district:new FormControl('',[Validators.required]),
+    state:new FormControl('',[Validators.required]),
+    city:new FormControl('',[Validators.required]),
+    landmark:new FormControl('',[Validators.required]),
+    blockno:new FormControl('',[Validators.required]),
+    }),
 	previousloandetils:this.PreviousLoanDetails
 
   }),
-  // educationDetails: new FormGroup({
+   educationDetails: new FormGroup({
+    eid:new FormControl('',[Validators.required]),
+    type:new FormControl('',[Validators.required]),
+    country:new FormControl('',[Validators.required]),
+    duration:new FormControl('',[Validators.required]),
+    fees :new FormControl('',[Validators.required]),
+    institutename:new FormControl('',[Validators.required]),
+     college:this.College
 
-
-  // })
-  // ,
+   })
+   ,
   // studentbankDetails: new FormGroup({
 
   // }),
@@ -73,14 +102,14 @@ this.application=new FormGroup({
 })
 }
 
-address=new FormGroup({
-  id:new FormControl('',[Validators.required]),
-  district:new FormControl('',[Validators.required]),
-  state:new FormControl('',[Validators.required]),
-  city:new FormControl('',[Validators.required]),
-  landmark:new FormControl('',[Validators.required]),
-  blockno:new FormControl('',[Validators.required]),
-  });
+// address=new FormGroup({
+//   id:new FormControl('',[Validators.required]),
+//   district:new FormControl('',[Validators.required]),
+//   state:new FormControl('',[Validators.required]),
+//   city:new FormControl('',[Validators.required]),
+//   landmark:new FormControl('',[Validators.required]),
+//   blockno:new FormControl('',[Validators.required]),
+//   });
 
   CIBIL=new FormGroup({
     cibilId:new FormControl('',[Validators.required]),
